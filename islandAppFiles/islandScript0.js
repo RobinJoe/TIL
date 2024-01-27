24b36d1d555ad882b875ab25b7b205b95653670b
RobinJoe <josephremail@gmail.com>
2024-01-27 21:09:36 +1000

    Adding a second event listener and improving the event listeners so they actually work

--- islandAppFiles/islandScript0.js
+++ islandAppFiles/islandScript0.js
@@ -1,3 +1,26 @@
-const islandItemWriter = document.createElement("div");
-islandItemWriter.innerText = "We started to add island information";
-document.body.appendChild(islandItemWriter);
+//const islandItemWriter = document.createElement("div");
+//islandItemWriter.innerText = "We started to add island information";
+//document.body.appendChild(islandItemWriter);
+
+addEventListener('change', function(e) {
+    document.getElementById('islandContainer').innerHTML = ""; //Clear the contents
+
+    var season = document.getElementById('seasonSelect'); //Get customer first selection: season
+    var IslandWriter0a = document.createElement('div'); //Set up selection in new div element
+    IslandWriter0a.innerHTML = season.value
+
+    var islandContainer0a = document.getElementById("islandContainer");  //Get the island information container
+    islandContainer0a.appendChild(IslandWriter0a); //Apply it to the element
+
+});
+
+addEventListener('change', function(e) {
+    document.getElementById('island2ndContainer').innerHTML = ""; //Clear the population size
+
+    var size = document.getElementById('sizeSelect'); //Get customer second select: size
+    var IslandWriter1b = document.createElement('div'); //Set up the selection in a new div element
+    IslandWriter1b.innerHTML = size.value
+
+    var islandContainer1b = document.getElementById("island2ndContainer"); //Get the 2nd island information container
+    islandContainer1b.appendChild(IslandWriter1b); //Apply it to the element
+})
